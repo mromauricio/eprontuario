dd09621b7ef25ac64241aff9bd97b735bb2d9801
Mauricio <mromauricio@gmail.com>
2020-11-29 18:20:27 -0300

    https

--- global/script/fetch.js
+++ global/script/fetch.js
@@ -77,9 +77,9 @@ async function PutAtualizaDataPaciente (data){
 
 async function GetNome(nome){  
   // let url =  new URL('http://localhost:9001/pacientes/nome');
-  // let url =  new URL('http://ec2-18-228-154-187.sa-east-1.compute.amazonaws.com:9001/pacientes/nome');
+  let url =  new URL('https://ec2-18-228-154-187.sa-east-1.compute.amazonaws.com:9001/pacientes/nome');
   // let url =  new URL('https://18.228.154.187:9001/pacientes/nome');
-  let url =  new URL('https://172.31.10.255/pacientes/nome');
+  // let url =  new URL('https://172.31.10.255/pacientes/nome');
   url.href += (`/?nome=${nome}`);
   try {
     let response = await fetch(url);
